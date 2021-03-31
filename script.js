5e6fc7fee77ebab69236c9c04a1cb71975b9f88f
Den <ghost198119@gmail.com>
2021-04-01 01:04:52 +0300

    movieDB

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const a = prompt('Один из последних просмотренных фильмов',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов',''),
+      d = prompt('На сколько оцените его?','');
+
+const personalMovieDB = {
+  'count': numberOfFilms,
+  'movies': {},
+  'actors': {},
+  'genres': [],
+  'privat': false
+};
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+
+console.log(personalMovieDB);
