578d5346cb0365862011bd3471c8779c28bec617
Den <ghost198119@gmail.com>
2021-04-01 16:13:54 +0300

    condition and loops

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 'use strict';
+
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const a = prompt('Один из последних просмотренных фильмов',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов',''),
-      d = prompt('На сколько оцените его?','');
 
 const personalMovieDB = {
   'count': numberOfFilms,
@@ -15,9 +13,28 @@ const personalMovieDB = {
 };
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i=0; i<2; i++){
+  const a = prompt('Один из последних просмотренных фильмов',''),
+      b = prompt('На сколько оцените его?','');
 
+      if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+      }else{
+        console.log('error');
+        i--;
+      }
+      
+}
 
+if(personalMovieDB.count < 10){
+  console.log('Просмотрено довольно мало фильмов');
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+  console.log('Вы классический зритель');
+}else if(personalMovieDB.count >= 30){
+  console.log('Вы киноман');
+}else{
+  console.log('Произошла ощибка');
+}
 
 console.log(personalMovieDB);
